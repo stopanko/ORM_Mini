b797a3ce47a2e2ce3f223c853606efd672677960
beatm1x <lesha0-lesha0@mail.ru>
2013-05-08 14:28:51 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ORMMini::Application.config.secret_token = 'bcc2dbe15b23d64279d599251bb68b05b317942a22d5b827f04932b5b54f1ed2b7e5813afbf4790d095ce6ca00c9ffb2b3a3845771a98c50109ecd78fb99ef3b'
